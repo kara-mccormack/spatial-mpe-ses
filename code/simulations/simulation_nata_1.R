94547ef94e46c8771ce469649152b3c3d3742a1f
Kara McCormack <karamccormack@Karas-MacBook-Air.local>
2019-12-12 16:12:09 -0500

    simulation_nata_1.R: did profile regression 20k sweeps using 10, 35, 50, 70 initial clusters

--- code/simulations/simulation_nata_1.R
+++ code/simulations/simulation_nata_1.R
@@ -11,8 +11,10 @@
 
 # Goals:
 # 1. Run a simulation of bayesian profile regression using the 
-#     PReMiuM package on the NATA data. Use 50000, 75000, and 100000
-#     sweeps and create a final boxplot of the results
+#     PReMiuM package on the NATA data. Examine plot of number
+#     of clusters versus number of sweeps to get an idea if
+#     the model converged.
+
 # load libraries
 library(PReMiuM)
 library(stringr)
@@ -24,6 +26,7 @@ data.dir = "/Users/karamccormack/Box/SES-environment/Spatial LCM Paper/Data/"
 output.dir = "/Users/karamccormack/Box/SES-environment/Spatial LCM Paper/v2/spatial-mpe-ses/output/scratch/scratch1/"
 # setwd to where the output (.txt) files should go
 setwd(output.dir)
+outputPlots.dir = "/Users/karamccormack/Box/SES-environment/Spatial LCM Paper/v2/spatial-mpe-ses/output/simulation-1/"
 
 # load log-transformed NC NATA data
 # log-transformed data comes from data.dir
@@ -33,53 +36,76 @@ NC_df_log = read.csv(file.path(data.dir, "NC_NATA_wide_log_transform.csv"))[,-1]
 # do not include the first column since this is 'Tract'
 covariate_names <- colnames(NC_df_log)[-1]
 
-# simulation 1
-n = 1 # set number of simulations desired
-# set prior values
-set.seed(678)
-# create reproducible vector of seeds for simulation
-# seed vector will be of length n 
-seed = sample(1:999999, size = n, replace = F)
+# define inputs for function
+seed = 898 # set random seed
 nBurn = 10000 # number of burn iterations
 nSweeps = 20000 # number of sweeps of MCMC algorithm
 data = NC_df_log # assign log transformed dataset
-output = "output" # assign prefix for temporary output .txt files
+output = "output_scratch" # assign prefix for temporary output .txt files
 covNames = covariate_names # assign covariate names from data frame
-num_clusters = rep(0,n) # create empty vector for final # of clusters found
+nClusInit = 35
+plotBySweep = 20
 
-# run with 80000 sweeps
-  for(i in 1:n){
-    runInfoObj = profRegr(yModel = "Normal", 
-                          xModel = "Normal",
-                          nSweeps = nSweeps,
-                          nBurn = nBurn,
-                          data = NC_df_log,
-                          output = "output",
-                          covNames = covariate_names,
-                          nClusInit = 35,
-                          whichLabelSwitch="123",
-                          run = TRUE,
-                          excludeY = TRUE, 
-                          seed = seed[i])
-    dissimObj = calcDissimilarityMatrix(runInfoObj)
-    clusObj = calcOptimalClustering(dissimObj)
-    num_clusters_1[i] <- max(unique(clusObj$clustering))
-  }
+myFunction = function(nClusInit){
+  runInfoObj = profRegr(yModel = "Normal", 
+                        xModel = "Normal",
+                        nSweeps = nSweeps,
+                        nBurn = nBurn,
+                        data = data,
+                        output = output,
+                        covNames = covNames,
+                        nClusInit = nClusInit,
+                        whichLabelSwitch="123",
+                        run = TRUE,
+                        excludeY = TRUE, 
+                        seed = seed)
+  # plot the number of clusters by number of sweeps
+  nClustersSweep<-read.table(paste0(output, "_nClusters.txt"))
+  nClusters_dat = as.data.frame(cbind(sweeps = seq(1:nSweeps), 
+                                      nClusters = nClustersSweep), 
+                                      nClusInit = rep(nClusInit, nSweeps))
+  # subset every 20th sweep
+  nClusters_dat2 = nClusters_dat[seq(1,nSweeps, by = plotBySweep),]
+  return(nClusters_dat2)
+}
 
-# plot the number of clusters by number of sweeps
-nClustersSweep<-read.table("output_nClusters.txt")
-head(nClustersSweep)
-nClusters_dat = as.data.frame(cbind(sweeps = seq(1:20000), nClusters = nClustersSweep))
-# try subsetting every 20th sweep
-nClusters_dat2 = nClusters_dat[seq(1,20000, by = 20),]
-p <- ggplot(data = nClusters_dat2, 
+nClusInitVec = c(10, 35, 50, 70)
+b = rbind(lapply(nClusInitVec, myFunction))
+b1 = cbind(b[[1]], nclusinit = rep(nClusInitVec[1], dim(b[[1]])[1]))
+b2 = cbind(b[[2]], nclusinit = rep(nClusInitVec[2], dim(b[[2]])[1]))
+b3 = cbind(b[[3]], nclusinit = rep(nClusInitVec[3], dim(b[[3]])[1]))
+b4 = cbind(b[[4]], nclusinit = rep(nClusInitVec[4], dim(b[[4]])[1]))
+b_full = rbind(b1, b2, b3, b4)
+
+# create plot of results
+p <- ggplot(data = b_full, 
             aes(x = sweeps, y = V1)) + 
+  facet_wrap(~nClusInit) +
   geom_line(color = "#00AFBB", size = .5) + 
   stat_smooth(color = "#FC4E07", 
               fill = "#FC4E07",
               method = "loess") +
-  labs(y="Number of Clusters", x = "Number of Sweeps")
-p
+  labs(y="Number of Clusters", x = "Number of Sweeps (After Burn-In)") +
+  annotate("text", x = nSweeps/10, y = max(nClusters_dat2$V1)-3, label = paste0("nClusInit = ", nClusInit)) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# End of Simulation 1
+########################################
 
 # run a second time with 100,000 sweeps
 nSweeps_2 = 100000
